8ff17c00e538219d93737f5717a91ff89c8f8ba9
Carlota Segura-Garcia <carlotasegurag@gmail.com>
2026-04-29 12:05:08 +0200

    Updated project directory structure.

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
+# https://data.nasa.gov/resource/eva.json (with modifications)
+
 
 def main(input_file, output_file, graph_file):
     print("--START--")
@@ -127,13 +129,13 @@ def add_duration_hours(df):
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        input_file = './eva-data.json'
-        output_file = './eva-data.csv'
+        input_file = './data/eva-data.json'
+        output_file = './results/eva-data.csv'
         print(f'Using default input and output filenames')
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
         print('Using custom input and output filenames')
 
-    graph_file = './cumulative_eva_graph.png'
+    graph_file = './results/cumulative_eva_graph.png'
     main(input_file, output_file, graph_file)
