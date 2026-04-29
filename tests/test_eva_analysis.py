03d10934cc213bbbaa6c69c55c4412e1b9d99292
Carlota Segura-Garcia <carlotasegurag@gmail.com>
2026-04-29 17:04:01 +0200

    Tested and corrected crew size calculation.

--- tests/test_eva_analysis.py
+++ tests/test_eva_analysis.py
@@ -1,5 +1,5 @@
 import pytest
-from eva_data_analysis import text_to_duration
+from eva_data_analysis import text_to_duration, calculate_crew_size
 
 def test_text_to_duration_float():
     """
@@ -18,3 +18,34 @@ def test_text_to_duration_integer():
     actual_result = text_to_duration("10:00")
     expected_result = 10
     assert actual_result == expected_result
+
+
+def test_calculate_crew_size():
+    """
+    Test that calculate_crew_size returns expected size of crew entries,
+    which is a string of names separated by semicolons
+    """
+
+    # Typical value 1
+    actual_result = calculate_crew_size("Carlota Segura Garcia; Hugues Gesbert")
+    expected_result = 2
+    assert actual_result == expected_result
+
+    # Typical value 2
+    actual_result = calculate_crew_size("Carlota Segura Garcia; Hugues Gesbert;")
+    expected_result = 2
+    assert actual_result == expected_result
+
+    # Typical value 2
+    actual_result = calculate_crew_size("Carlota Segura Garcia; Hugues Gesbert;;")
+    expected_result = 2
+    assert actual_result == expected_result
+
+# Edge cases
+def test_calculate_crew_size_edge_cases():
+    """
+    Test that calculate_crew_size returns expected ground truth values
+    for edge case where crew is an empty string
+    """
+    actual_result = calculate_crew_size("")
+    assert actual_result is None
